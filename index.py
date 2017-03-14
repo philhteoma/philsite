97f12d08b37be72b6222c8ff0b729e7018b06a99
philh <philharrison99@gmail.com>
2017-03-14 13:27:06 +0000

    Getting index page up and running

--- index.py
+++ index.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("projects.html")
+    return render_template("index.html")
 
 @app.route("/projects")
 def projects():
